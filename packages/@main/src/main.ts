18f29e28cde4b053c758d5317c6c45d40d10c918
rethred <gabrielwastha@gmail.com>
2024-02-22 18:06:38 -0300

    make diagram

--- packages/@main/src/main.ts
+++ packages/@main/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log("maind!",)
+  console.log("maidnd!",)
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
 }
