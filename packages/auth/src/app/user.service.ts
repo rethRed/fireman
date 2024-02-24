c7a661aecdad5f0f93d7f44075849f790058d323
rethred <gabrielwastha@gmail.com>
2024-02-24 15:14:15 +0000

    save

--- packages/auth/src/app/user.service.ts
+++ packages/auth/src/app/user.service.ts
@@ -5,7 +5,7 @@ import { EmailAlreadyInUseError, UsernameAlreadyInUseError } from "./errors";
 import { Either, failure, success } from "@fireman/common/logic";
 import { TypeORM } from "./@shared";
 import { DomainError } from "@fireman/common/errors";
-//ss
+//ssd
 @Injectable()
 export class UserService {
     
